2564d6946b6ef4f659e8135c051e7c6463b03f00
Nmk78 <naymyokhant78@gmail.com>
2024-11-05 20:46:57 +0630

    Removed Redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Nav } from "@/components/Nav";
-import { ClerkProvider, SignedOut, SignInButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { DataProvider } from "@/context/DataContext";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import { ReactQueryProvider } from "./providers";
