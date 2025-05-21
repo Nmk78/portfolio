8a09340e969e2ba6d606a3d9087637e6aa3d2f9a
Nmk78 <naymyokhant78@gmail.com>
2025-05-21 22:03:54 +0630

    fixing redirects for seo

--- next.config.mjs
+++ next.config.mjs
@@ -2,28 +2,59 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
 
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; img-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=()",
+          },
+        ],
+      },
+    ];
+  },
+
   async redirects() {
-    const domain = process.env.url || 'https://www.naymyokhant.online'
+    const domain = process.env.url || "https://www.naymyokhant.online";
 
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         has: [
           {
-            type: 'host',
+            type: "host",
             value: domain,
           },
         ],
         destination: `${domain}/:path*`,
         permanent: true,
       },
-    ]
+    ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
