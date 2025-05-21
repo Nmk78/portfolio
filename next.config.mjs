8d75461cb5ac05148a7b5f202cfc30981cdb5204
Nmk78 <naymyokhant78@gmail.com>
2025-05-21 21:31:30 +0630

    fixing redirects for seo

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**', // Allow images from any domain
-        },
-        {
-          protocol: 'http',
-          hostname: '**', // If you want to allow images from any HTTP domain as well
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
+    ],
+  },
+
+  async redirects() {
+    const domain = process.env.url || 'https://www.naymyokhant.online'
+
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: domain,
+          },
+        ],
+        destination: `${domain}/:path*`,
+        permanent: true,
+      },
+    ]
+  },
+}
+
+export default nextConfig
