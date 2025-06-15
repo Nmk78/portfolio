acb277a337c8d23fd04f35872dfb37f09aff263e
Nmk78 <naymyokhant78@gmail.com>
2025-06-15 15:42:38 +0630

    Rag Chat Modal change

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -4,50 +4,7 @@ import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { prisma } from "@/lib/prisma";
 import { redis } from "@/lib/redis";
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     // Parse the request body
-//     const { sessionId, messages } = await request.json();
-//     console.log("🚀 ~ messages:", messages);
 
-//     const lastMessage = messages[messages.length - 1].content;
-//     console.log("🚀 ~ lastMessage:", lastMessage);
-
-//     if (!sessionId) {
-//       return NextResponse.json(
-//         {
-//           status: "error",
-//           message: "Session ID is missing.",
-//           data: null,
-//         },
-//         { status: 4000 }
-//       );
-//     }
-
-//     const res = await ragChat.chat(lastMessage, { streaming: true, sessionId });
-//     console.log("🚀 ~ res:", res);
-
-//     // const responseEnvelope = {
-//     //   status: "success",
-//     //   message: "Response from chatbot",
-//     //   data: res,
-//     // };
-
-//     return aiUseChatAdapter(res);
-//   } catch (error) {
-//     console.error("Chatbot error:", error);
-
-//     return NextResponse.json(
-//       {
-//         status: "error",
-//         message: "Failed to get response from chatbot.",
-//         error: error,
-//         data: null,
-//       },
-//       { status: 500 }
-//     );
-//   }
-// }
 
 export async function POST(request: NextRequest) {
   try {
@@ -144,3 +101,48 @@ export async function POST(request: NextRequest) {
     );
   }
 }
+
+// export async function POST(request: NextRequest) {
+//   try {
+//     // Parse the request body
+//     const { sessionId, messages } = await request.json();
+//     console.log("🚀 ~ messages:", messages);
+
+//     const lastMessage = messages[messages.length - 1].content;
+//     console.log("🚀 ~ lastMessage:", lastMessage);
+
+//     if (!sessionId) {
+//       return NextResponse.json(
+//         {
+//           status: "error",
+//           message: "Session ID is missing.",
+//           data: null,
+//         },
+//         { status: 4000 }
+//       );
+//     }
+
+//     const res = await ragChat.chat(lastMessage, { streaming: true, sessionId });
+//     console.log("🚀 ~ res:", res);
+
+//     // const responseEnvelope = {
+//     //   status: "success",
+//     //   message: "Response from chatbot",
+//     //   data: res,
+//     // };
+
+//     return aiUseChatAdapter(res);
+//   } catch (error) {
+//     console.error("Chatbot error:", error);
+
+//     return NextResponse.json(
+//       {
+//         status: "error",
+//         message: "Failed to get response from chatbot.",
+//         error: error,
+//         data: null,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
--- lib/rag-chat.ts
+++ lib/rag-chat.ts
@@ -5,7 +5,8 @@ import { redis } from "./redis";
 //   "Respond like Nay Myo Khant: Keep it friendly and straightforward.";
 
 export const ragChat = new RAGChat({
-  model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+  model: upstash("mistralai/Mistral-7B-Instruct-v0.2"),
+  // model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
   redis: redis,
   debug: true,
 });
